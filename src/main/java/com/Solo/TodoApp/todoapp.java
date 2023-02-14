c8434bcd809d37598991744d4201f87d9904a965
jiyoonchol <jiyoon276479@gmail.com>
2023-02-14 21:27:08 +0900

    main commit

---
+++ src/main/java/com/Solo/TodoApp/todoapp.java
@@ -0,0 +1,13 @@
+package com.Solo.TodoApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class todoapp {
+    @GetMapping("/")
+    public String getToDoApp() {
+        return "To-do Application~";
+    }
+}
